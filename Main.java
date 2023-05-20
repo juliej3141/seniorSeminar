112e7f3c4fa84360bb668582b4b99891469d17c0
juliej3141 <98037714+juliej3141@users.noreply.github.com>
2023-05-20 13:24:42 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    SeniorSeminar ss1 = new SeniorSeminar();
+    ss1.load();
+    //s1.schedule();
+    //s1.printA();
+    ss1.setPrio();
+    ss1.run();
+
+  }
+}
